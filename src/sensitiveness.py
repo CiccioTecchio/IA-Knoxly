a9eaaa0a150d7df35fb224b5dc793186cb9360b7
CiccioTecchio <fr.vicidomini94@gmail.com>
2020-04-21 17:33:33 +0200

    added roc_auc & confusion mtr, balancing ds200

--- src/sensitiveness.py
+++ src/sensitiveness.py
@@ -8,11 +8,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import precision_recall_fscore_support as score
-from sklearn.metrics import auc, accuracy_score, roc_auc_score, roc_curve
-
+from sklearn.metrics import confusion_matrix, auc, accuracy_score, roc_auc_score, roc_curve
+from matplotlib import cm
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-def sensitiveness(ds, n_folds, scoring, path_RF, path_training):
+def sensitiveness(ds, n_folds, scoring, path_RF, path_training, path_fig, title):
     colnames = ['text', 'tipo', 'sensibile']
     #filename = open('src/ds200.csv','r')
     filename = open(ds ,'r')
@@ -45,12 +45,72 @@ def sensitiveness(ds, n_folds, scoring, path_RF, path_training):
     fp.write(misure)
     fp.write("roc_auc: "+str(roc_auc))
     fp.close()
+    
+    plot_confusion_matrix(test_set_labels, y_pred, title=title, path_matr = path_fig+"mtr_"+title+".png")
+    return classificatore
 
-    #roc auc
-    #https://stackoverflow.com/questions/43043271/roc-curve-for-binary-classification-in-python
+def plot_confusion_matrix(y_true, y_pred, classes=[0, 1],
+                          normalize=True,
+                          title = None,
+                          cmap=cm.Blues,
+                          path_matr= None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    print('conf matrix: {}'.format(cm))
+    # Only use the labels that appear in the data
+    # classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    fig.savefig(path_matr)
+    return ax
+
+    
+#https://stackoverflow.com/questions/43043271/roc-curve-for-binary-classification-in-python
+def plot_roc_auc(y_true, y_pred,path_fig):
     fpr, tpr, auc_plot = dict(), dict(), dict()
     for i in range(2):
-        fpr[i], tpr[i], _ = roc_curve(test_set_labels, y_pred)
+        fpr[i], tpr[i], _ = roc_curve(y_true, y_pred)
         auc_plot[i] = auc(fpr[i], tpr[i])
     plt.figure()
     plt.plot(fpr[1], tpr[1], color='darkorange', lw = 2, label='ROC curve (area = %0.2f)' % auc_plot[1])
@@ -61,4 +121,6 @@ def sensitiveness(ds, n_folds, scoring, path_RF, path_training):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver operating characteristic example')
     plt.legend(loc = "lower right")
-    plt.show()
+    plt.savefig(path_fig+"roc_auc.png")
+    #plt.show()
+    plt.close()
--- src/sensitivenessOnDs.py
+++ src/sensitivenessOnDs.py
@@ -1,27 +1,16 @@
-import sensitiveness
+import sensitiveness, pickle
+
+PATH_TOPIC = "src/topic_ds/"
+title = ["politics", "health", "job", "travel", "general"]
+path = []
+for i in title: path.append(PATH_TOPIC+i+"/")
+for i in range(5):
+    print(title[i])
+    classificatore = sensitiveness.sensitiveness(path[i]+title[i]+"200.csv", 10, 'roc_auc', path[i]+"RF.txt", path[i]+"misure_testing.txt", path[i], title[i])
+    pickle.dump(classificatore, open(path[i]+title[i]+".dump", "wb"))
 
 #sensitiveness on DS
 PATH = "src/sensitiveness_class/"
-sensitiveness.sensitiveness('src/ds200.csv', 10, 'roc_auc', PATH+"RF.txt", PATH+"training/misure_testing.txt")
-
-# politics
-#PATH_TOPIC = "src/topic_ds/"
-#sensitiveness.sensitiveness(PATH_TOPIC+"politics200.csv", 10, 'roc_auc', PATH_TOPIC+"politics/RF.txt", PATH_TOPIC+"politics/misure_testing.txt")
-
-# health
-#sensitiveness.sensitiveness(PATH_TOPIC+"health200.csv", 10, 'roc_auc', PATH_TOPIC+"health/RF.txt", PATH_TOPIC+"health/misure_testing.txt")
-
-# jobs
-#sensitiveness.sensitiveness(PATH_TOPIC+"job200.csv", 10, 'roc_auc', PATH_TOPIC+"job/RF.txt", PATH_TOPIC+"job/misure_testing.txt")
-
-# travel
-#sensitiveness.sensitiveness(PATH_TOPIC+"travel200.csv", 10, 'roc_auc', PATH_TOPIC+"travel/RF.txt", PATH_TOPIC+"travel/misure_testing.txt")
-
-# general
-#sensitiveness.sensitiveness(PATH_TOPIC+"general200.csv", 10, 'roc_auc', PATH_TOPIC+"general/RF.txt", PATH_TOPIC+"general/misure_testing.txt")
-
-
-#Plot roc_auc
-
-#Print matrici di confusione
-
+print("ds200")
+classificatore = sensitiveness.sensitiveness('src/ds200.csv', 10, 'roc_auc', PATH+"RF.txt", PATH+"training/misure_testing.txt", PATH, "ds200")
+pickle.dump(classificatore, open(PATH+"ds200.dump", "wb"))
